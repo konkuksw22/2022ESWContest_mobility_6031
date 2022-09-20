16a19e88687979a7a991cf70ba86ff4dc5d0f059
seoyeon8167 <102856585+seoyeon8167@users.noreply.github.com>
2022-09-20 22:05:03 +0900

    Update final.py

--- perspective_transform/final.py
+++ perspective_transform/final.py
@@ -20,49 +20,40 @@ def perspective_transform(img, pts, title):
   
     result = cv2.warpPerspective(img, mtrx, (width, height))
     cv2.imshow(title, result)
-
-def move(dir,x):
-    if keyboard.is_pressed("d") :
-        if x==2: 
-            dir[1][1] = dir[1][1] - 10
-            dir[3][1] = dir[3][1] + 10
-        elif x==0:
-            dir[0][1] = dir[0][1] + 10
-            dir[2][1] = dir[2][1] - 10
-        print('d')
-    elif keyboard.is_pressed("w"):
-        for i in range (0,4):
-            dir[i][1]=dir[i][1] + 10
-        print('w')
-    elif keyboard.is_pressed("a"):
-        if x==2: 
-            dir[1][1] = dir[1][1] + 10
-            dir[3][1] = dir[3][1] - 10
-        elif x==0:
-            dir[0][1] = dir[0][1] - 10
-            dir[2][1] = dir[2][1] + 10
-        print('a')
-    elif keyboard.is_pressed("x"):
-        for i in range (0,4):
-            dir[i][1]=dir[i][1] - 10
-        print('x')
     
-def getsize(capture1, capture2):
+def getsize(capture1,capture2):
     re_D, img_D = capture1.read()
     re_B, img_B = capture2.read()
-
-    img_D = cv2.resize(img_D, (1024, 576))
-    img_B = cv2.resize(img_B, (1280, 576))
+    img_D = cv2.resize(img_D, (1280,720))
+    img_B = cv2.resize(img_B, (1280,720))
     cv2.imshow("image", img_D)
-
-    for i in range (0,3):
+    for i in range (3):
         while True:
-            perspective_transform(img_B, dir[0], "tl")
-            perspective_transform(img_B, dir[2], "tr")
-
-            move(dir[i],i)
+            perspective_transform(img_B, dir[0],"tl")
+            perspective_transform(img_B, dir[2],"tr")
+            if keyboard.is_pressed("d"):
+                if i==0:
+                    dir[i][0][1] = dir[i][0][1] + 10
+                    dir[i][2][1] = dir[i][2][1] - 10
+                elif i==2:
+                    dir[i][1][1] = dir[i][1][1] + 10
+                    dir[i][3][1] = dir[i][3][1] - 10
+            elif keyboard.is_pressed("w"):
+                for j in range (0,4):
+                    dir[i][j][1]=dir[i][j][1] + 10
+            elif keyboard.is_pressed("a"):
+                if i==0:
+                    dir[i][0][1] = dir[i][0][1] - 10
+                    dir[i][2][1] = dir[i][2][1] + 10
+                elif i==2:
+                    dir[i][1][1] = dir[i][1][1] - 10
+                    dir[i][3][1] = dir[i][3][1] + 10
+            elif keyboard.is_pressed("x"):
+                for j in range (0,4):
+                    dir[i][j][1]=dir[i][j][1] - 10
             if cv2.waitKey() == ord('q'):
-                break    
+                break
+    
     np.save('dir.npy', dir)
     cv2.destroyAllWindows()
     flag = 1
@@ -72,28 +63,37 @@ flag=0
 if os.path.isfile("./dir.npy")==True:
     dir=np.load('dir.npy')
 else :
-    right=[[1280,0],[1030,120],[1280,720],[1030,650]]
+    right=[[1030,0],[1280,120],[1030,720],[1280,650]]
     left=[[0,120],[250,0],[0,620],[250,720]]
     nom=[[0,0],[0,0],[0,0],[0,0]]
     dir=[left,nom,right]
 
 cap_builtin = cv2.VideoCapture("./builtin_3.mp4")
-cap_driver = cv2.VideoCapture("./driver_3.mp4")
+cap_driver = cv2.VideoCapture("./builtin_3.mp4")
+flag = getsize(cap_builtin,cap_driver)
 
-flag = getsize(cap_driver,cap_builtin)
+# cap2 = cv2.VideoCapture("220817_blackbox2.mp4")
+while cap_builtin.isOpened() and cap_driver.isOpened() and flag == 1 :
+    ret, frame = cap_builtin.read()
+    ret1, frame1 = cap_driver.read()
+    rows, cols = frame.shape[:2]
 
-while cap_driver.isOpened() and cap_builtin.isOpened() and flag==1 :
-    ret, frame = cap_driver.read()
-    ret1,frame1= cap_builtin.read()
+    # ret2, frame2 = cap2.read()
 
-    cv2.imshow("video", frame)
-
-    perspective_transform(frame1, dir[0],"tl")
-    perspective_transform(frame1, dir[2],"tr")
+    cv2.imshow("video", frame1)
+    # cv2.imshow("video2", frame2)
+    perspective_transform(frame, dir[0],"tl")
+    perspective_transform(frame, dir[2],"tr")
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-   
+    # if cv2.waitKey(1) & 0xFF == ord('w'):
+    #     print('w')
+
+    # keycode = cv2.waitKey(1)
+    # if keycode == ord('w'):
+    #     print('w')
+
 cap_builtin.release()
 cap_driver.release()
 cv2.destroyAllWindows()
